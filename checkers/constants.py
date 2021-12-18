96a9f4bdb7fb07250a100b3202d9ae982336b07e
AhmedSamir1271 <egahmedsamir1271@gmail.com>
2021-12-18 11:06:02 +0200

    added new constant variables to constants file

---
+++ checkers/constants.py
@@ -0,0 +1,14 @@
+import pygame
+
+#Size of board
+WIDTH, HEIGHT = 800, 800 
+
+ROWS, COLS = 8, 8
+#Size of one square in the board
+SQUARE_SIZE = WIDTH//COLS 
+
+# RGB Color
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+Blue = (0,0,255)
+RED = (255, 0, 0)
