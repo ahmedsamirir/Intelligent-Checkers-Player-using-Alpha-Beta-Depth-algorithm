b603bedc782dc0779209bb8eb246aab462046d5c
AhmedSamir1271 <egahmedsamir1271@gmail.com>
2021-12-23 23:02:19 +0200

    Ai_algorithm file Creation

---
+++ Ai_algorithm/Alpha-Beta Depth-First algorithm.py
@@ -0,0 +1,17 @@
+import pygame
+
+
+#Start minimax_to_alphabeta algorithm
+def alpha_beta_algo():
+    pass
+#End minimax_to_alphabeta algorithm
+
+#Start simulate method
+def simulate_move():
+    pass
+#End simulate method
+
+#Start get_all_moves method
+def get_all_moves():
+    pass
+#End get_all_moves method
